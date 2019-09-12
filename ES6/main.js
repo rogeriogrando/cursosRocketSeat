8ed5f8b9550ab2b1e4d96b37245d3613752849c7
Rogério Grando <rgrando.unique@gmail.com>
2019-09-12 00:14:10 -0300

    Contunação ES6

--- ES6/main.js
+++ ES6/main.js
@@ -1,27 +1,4 @@
-class List {
-  constructor() {
-    this.data = [];
-  }
-  add(data) {
-    this.data.push(data);
-    console.log(data);
-  }
-}
+const usuario = { nome: "Rogerio" };
 
-class TodoList extends List {
-  constructor() {
-    super();
-    this.usuario = "Rogério";
-  }
-  mostraUsuario() {
-    console.log(this.usuario);
-  }
-}
-
-var MinhaLista = new TodoList();
-
-document.getElementById("novotodo").onclick = function() {
-  MinhaLista.add("Novo todo");
-};
-
-MinhaLista.mostraUsuario();
+usuario.nome = "Tais";
+console.log(usuario);
